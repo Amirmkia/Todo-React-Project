1284a1f91a7b349511776b515bf85cc84e439f95
amirmkia Ahmadi <amirmatmms@gmail.com>
2022-05-03 12:13:14 +0430

    first commit

---
+++ src/components/Task/common/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = ({ teachersName }) => {
+    return (
+        <div className="card text-center">
+            <div className="card-header">درباره سازنده</div>
+            <div className="card-body">
+                <h5 className="card-title">اپلیکیشن کارهای روزمره</h5>
+                <p className="card-text">برنامه نویس : امیر محمد احمدی</p>
+            </div>
+         
+        </div>
+    );
+};
+
+export default About;
